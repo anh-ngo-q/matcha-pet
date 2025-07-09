7607e973ba479588cd7035fc474a0455658e61a7
Anh Ngo <qanhngo.cs@gmail.com>
2025-07-09 00:00:05 -0400

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/matchapet/MatchaPetApplication.java
@@ -0,0 +1,13 @@
+package com.matchapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchaPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchaPetApplication.class, args);
+    }
+
+}
